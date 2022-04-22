cb5c4eac78eac68263911a582c2ccbee81c9c5c7
TeresaVu <95201928+TeresaVu@users.noreply.github.com>
2022-04-22 13:37:20 -0400

    Update

--- swe_storage/swe_storage.ino
+++ swe_storage/swe_storage.ino
@@ -4,8 +4,8 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 #include <SD.h>
-RF24 contr(2, 3);  // CE, CSN
-RF24 postn(4, 5);
+RF24 contr(4, 6);  // CE, CSN
+RF24 postn(3, 5);
 const byte address1[6] = "CONTR";
 const byte address2[6] = "POSTN";
 int chipSelect = 10;
@@ -16,6 +16,12 @@ int numQueries = 0; // the number of lfdm responses received
 int connectionCount = 0; // keep track of how many current lfdm connections there are
 int connectionLedPin = 6; // the LED pin numbers for the corresponding lfdm connections, 6-9 corresponds to lfdm 1-4 respectively
 
+struct empty_packet
+{
+  
+};
+empty_packet packet;
+
 struct position_update
 {
   float latitude;
@@ -25,14 +31,6 @@ struct position_update
 };
 position_update pos;
 
-struct lfdm_identifier
-{
-  char id[4]; // 3-letter string (unique ID)
-  byte address[6]; // 5-byte nRF24L01+ address
-};
-lfdm_identifier connections[4]; // contains the four potential lfdm connections
-lfdm_identifier testConnection;
-
 struct lfdm_detection_signal
 {
   float intensity;
@@ -40,29 +38,12 @@ struct lfdm_detection_signal
 };
 lfdm_detection_signal currentSignal;
 
-struct lfdm_query
-{
-  lfdm_identifier identifier;
-  lfdm_detection_signal signal;
-};
-lfdm_query query; // holds the response from the lfdm temporarily before being added into a new position_entry
-
-struct position_entry
-{
-  position_update pos;
-  int num_queries;
-  lfdm_query queries[];
-};
-position_entry newPositionEntry;
-
 void setup()
 {
   while (!Serial);
     Serial.begin(9600);
   contr.begin();
   postn.begin();
-  contr.enableAckPayload(); // allows contr module to accept acknowledge packets from lfdm
-  contr.setRetries(0, 2); // (delay, count) retries sending data to lfdm twice without delay
   contr.openReadingPipe(0, address1);
   postn.openReadingPipe(0, address2);
   contr.startListening();
@@ -79,12 +60,8 @@ void setup()
       Serial.println("Could not removed file.");
     }
   }
-  // lfdm connections:
-  pinMode(6, OUTPUT); // 1
-  pinMode(7, OUTPUT); // 2
-  pinMode(8, OUTPUT); // 3
-  pinMode(9, OUTPUT); // 4
-  pinMode(A0, OUTPUT);
+  pinMode(A1, OUTPUT); // lfdm connection
+  pinMode(A0, OUTPUT); // position
 }
 
 void loop() 
@@ -92,7 +69,6 @@ void loop()
   if (postn.available()) // read position updates from autonomy
   {
     digitalWrite(A0, HIGH);
-    char test[32] = {0};
     postn.read(&pos, sizeof(pos));
     firstPositionReceived = true;
     Serial.println("Position received");
@@ -100,92 +76,32 @@ void loop()
     Serial.println(pos.longitude);
     Serial.println(pos.altitude);
     Serial.println(pos.timestamp);
-    
     digitalWrite(A0, LOW);
+
+    if (contr.available()) {
+      digitalWrite(A1, HIGH);
+      contr.read(&currentSignal, sizeof(currentSignal));
+      digitalWrite(A1, LOW);
+    }
   } 
-  
-  
-  if (true) // !firstPositionReceived while first position has not been received AND max connections (4) not yet reached, listen for lfdm connections
-  {
-    if (contr.available() && connectionCount < 4)
-    {
-      boolean connectionExists[4] = {(connectionCount>=1), (connectionCount>=2), (connectionCount>=3), (connectionCount>=4)};
-      boolean ignore = false;
-      contr.read(&testConnection, sizeof(testConnection)); // stores lfdm info in the lfdm_identifier struct 
-      for (int i = 0; i < connectionCount; i++) {
-        for (int j = 0; j < 3; j++) {
-          if (testConnection.id[j] != connections[i].id[j]) {
-            connectionExists[i] = false;
-          }
-        }
-      }
-      for (int i = 0; i < sizeof(connectionExists); i++) {
-        if (connectionExists[i] == true) {
-          ignore = true;
-        }
-      }
-      if (!ignore) {
-        for (int i = 0; i < sizeof(testConnection.id); i++) {
-          connections[connectionCount].id[i] = testConnection.id[i];
-        }
-        for (int i = 0; i < sizeof(testConnection.address); i++) {
-          connections[connectionCount].address[i] = testConnection.address[i];
-        }
-        connectionCount++;
-        digitalWrite(connectionLedPin, HIGH); // turn on LED for each corresponding lfdm connection
-        connectionLedPin++;
-        Serial.print("Connection received: ");
-        Serial.println(connectionCount);
-        Serial.println(connections[connectionCount].id);
-      }
-    }
-  }
-  else
-  {
-    for (lfdm_identifier lfdm : connections) { // write an empty packet to each lfdm address
-      contr.openWritingPipe(lfdm.address);
-      contr.stopListening();
-      send = contr.write(&pos, sizeof(pos)); // FIXME Figure out how to write an empty packet to lfdm
-      delay(1000);
-      if (send) { // if a packet is successfully sent, read the acknowledge packet that lfdm sends back, which contains the current detection signal
-          if (contr.isAckPayloadAvailable()) {
-              contr.read(&currentSignal, sizeof(currentSignal));
-              numQueries++;
-              query = {lfdm, currentSignal}; // store the response from each lfdm
-              newPositionEntry.queries[numQueries - 1] = query; // adds the lfdm response to a new position entry
-          }
-          else {
-              Serial.println("Acknowledge but no data");
-          }
-      }
-      else {
-          Serial.println("Tx failed");
-      }
-    }
-    newPositionEntry.pos = pos;
-    newPositionEntry.num_queries = numQueries;
-    add_position_entry(newPositionEntry);
-  }
+  add_position_entry(pos, currentSignal);
 }
 
-void add_position_entry(position_entry entry)
+void add_position_entry(position_update entry, lfdm_detection_signal currentSignal)
 {
-    file = SD.open("file.txt", FILE_WRITE); // open "file.txt" to write data
+  file = SD.open("file.txt", FILE_WRITE); // open "file.txt" to write data
   if (file) {
     file.print("NEW_ENTRY ");
-    file.println(entry.pos.timestamp);
+    file.println(entry.timestamp);
     file.print("LOCATION ");
-    file.print(entry.pos.latitude);
-    file.print(entry.pos.longitude);
-    file.println(entry.pos.altitude);
-    for (int i = 0; i < entry.num_queries; i++) {
-      file.print("LFDM_SIGNAL ");
-      file.print(entry.queries[i].identifier.id);
-      file.print(entry.queries[i].signal.intensity);
-    }
+    file.print(entry.latitude);
+    file.print(entry.longitude);
+    file.println(entry.altitude);
+    file.println(currentSignal.intensity);
+    file.println(currentSignal.depth);
     file.close(); // close file
     Serial.print("Wrote number: "); // debug output: show written number in serial monitor
-    Serial.println(entry.pos.latitude);
+    Serial.println(entry.latitude);
     
   } else {
     Serial.println("Could not open file (writing).");
@@ -211,10 +127,10 @@ void add_position_entry(position_entry entry)
 //#include <RF24.h>
 //
 ////create an RF24 object
-//RF24 radio(2, 3);  // CE, CSN
+//RF24 radio(3, 5);  // CE, CSN
 //
 ////address through which two modules communicate.
-//const byte address[6] = "00006";
+//const byte address[6] = "00013";
 //
 //void setup()
 //{
